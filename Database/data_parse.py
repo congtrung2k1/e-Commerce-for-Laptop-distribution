c52b438fa1d6653e390d41aedd5ccc454a0ac65f
Cong Trung <42043537+congtrung2k1@users.noreply.github.com>
2023-05-16 13:28:16 +0700

    update database

--- Database/data_parse.py
+++ Database/data_parse.py
@@ -21,17 +21,20 @@ with open('Amazon_Laptop_Specs.csv', 'r') as file:
             price = 30000.0
         else:
             price = float(decimal.Decimal(row[1]))
+
         if row[4] == '':
             date = '2022-07-05'
         else:
             date = datetime.strptime(row[4], "%d %B %Y").strftime("%Y-%m-%d")
         cate = row[0].split(' ')[0]
 
+        des = 'rank ' + ('unknown' if row[3] == '' else row[3]) + ' | ' + ('25.2 x 36.2 x 2 Centimeters' if row[7] == "" else row[7]) + ' | ' + ('4.3' if row[11] == "" else row[11])
+
         cursor = mydb.cursor()
         sql = """
                 INSERT INTO products (name, price, image, description, create_date, category) 
                 VALUES (%s, %s, %s, %s, %s, %s)
         """
-        value = (row[0], price, '', row[3], date, cate)
+        value = (row[0], price, '', des, date, cate)
         cursor.execute(sql, value)
         mydb.commit()
